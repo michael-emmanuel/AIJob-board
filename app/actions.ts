1a5e0626de0fd9d11c1bf855bf10bbe3586f58d1
Michael Emmanuel <michael.emmanuel732@gmail.com>
2025-05-01 01:30:51 -0400

    add arcjet proctection to app

--- app/actions.ts
+++ app/actions.ts
@@ -5,10 +5,33 @@ import { requireUser } from './utils/requireUser';
 import { companySchema, jobSeekerSchema } from './utils/zodSchemas';
 import { prisma } from './utils/db';
 import { redirect } from 'next/navigation';
+import arcjet, { detectBot, shield } from './utils/arcjet';
+import { request } from '@arcjet/next';
 
+const aj = arcjet
+  .withRule(
+    shield({
+      // see what it blocks ... docs.arcjet.com/shield/concepts
+      mode: 'LIVE',
+    })
+  )
+  .withRule(
+    detectBot({
+      mode: 'LIVE',
+      allow: [], // allow google seo, etc
+    })
+  );
 // By default a server action creates a public HTTP endpoint
 export async function createCompany(data: z.infer<typeof companySchema>) {
   const session = await requireUser();
+
+  const req = await request();
+
+  const decision = await aj.protect(req);
+
+  if (decision.isDenied()) {
+    throw new Error('Forbidden');
+  }
 
   const validateData = companySchema.parse(data);
 
