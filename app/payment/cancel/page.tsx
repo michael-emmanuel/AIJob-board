607c40e32d5976ed66b2c8ce9492bd87ceb80b93
Michael Emmanuel <michael.emmanuel732@gmail.com>
2025-05-01 15:05:50 -0400

    configure payment canceled page

---
+++ app/payment/cancel/page.tsx
@@ -0,0 +1,28 @@
+import { Button } from '@/components/ui/button';
+import { Card } from '@/components/ui/card';
+import { Check, XIcon } from 'lucide-react';
+import Link from 'next/link';
+
+export default function PaymentCancelled() {
+  return (
+    <div className='w-full min-h-screen flex flex-1 justify-center items-center'>
+      <Card className='w-[350px]'>
+        <div className='p-6'>
+          <div className='w-full flex justify-center'>
+            <XIcon className='size-12 p-2 bg-red-500/30 text-red-500 rounded-full' />
+          </div>
+          <div className='mt-3 text-center sm:mt-5 w-full'>
+            <h2 className='text-xl font-semibold'>Payment Canceled</h2>
+            <p className='text-xs mt-2 text-muted-foreground tracking-tight text-balance'>
+              No worries, you will not be charged. Please try again!
+            </p>
+
+            <Button asChild className='w-full mt-5'>
+              <Link href='/'>Go back to Homepage</Link>
+            </Button>
+          </div>
+        </div>
+      </Card>
+    </div>
+  );
+}
