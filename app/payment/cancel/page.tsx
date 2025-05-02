bde69a87005c34a16116fb65ba2a3d820c13073a
Michael Emmanuel <michael.emmanuel732@gmail.com>
2025-05-02 19:19:57 -0400

    secure routes that should only be viewed by logged in sure

--- app/payment/cancel/page.tsx
+++ app/payment/cancel/page.tsx
@@ -1,9 +1,12 @@
+import { requireUser } from '@/app/utils/requireUser';
 import { Button } from '@/components/ui/button';
 import { Card } from '@/components/ui/card';
 import { Check, XIcon } from 'lucide-react';
 import Link from 'next/link';
 
-export default function PaymentCancelled() {
+export default async function PaymentCancelled() {
+  await requireUser();
+
   return (
     <div className='w-full min-h-screen flex flex-1 justify-center items-center'>
       <Card className='w-[350px]'>
